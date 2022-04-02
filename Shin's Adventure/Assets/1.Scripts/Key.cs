82aa142e56ad8516e1ef394a504b510dce435eb6
ComeBiga <36800639+ComeBiga@users.noreply.github.com>
2022-04-02 17:32:55 +0900

    Continuing

---
+++ Shin's Adventure/Assets/1.Scripts/Key.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Key : MonoBehaviour, Interactive, OnRaycast
+{
+    public GameObject fitDoor;
+
+    public UnityEvent OnSelect;
+
+    public AudioSource pickUpSound;
+
+    public void Interact(GameObject g)
+    {
+        Debug.Log("Key");
+
+        OnSelect.Invoke();
+        SoundManager.instance.Play("PickUpKey");
+
+        gameObject.SetActive(false);
+    }
+
+    public void OnRaycasted()
+    {
+        GetComponent<cakeslice.Outline>().eraseRenderer = false;
+    }
+
+    public void OnRaycastExit()
+    {
+        GetComponent<cakeslice.Outline>().eraseRenderer = true;
+    }
+}
--- Shin's Adventure/Assets/_Scripts/Interactive.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface Interactive
-{
-    void Interact(GameObject g);
-}
